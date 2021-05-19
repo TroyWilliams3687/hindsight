8dec796e08f74f6469c3c2ed4ce51c1fdfffbf10
Troy Williams <troy.williams@bluebill.net>
2021-05-19 09:21:27 -0400

    The script now works properly. It wasn't placing things on the correct desktop. For this to work we had to apply the change in position, then the change to the correct desktop. This seems to work. refactored the script to make it a little cleaner.

--- hindsight/restore.py
+++ hindsight/restore.py
@@ -41,10 +41,138 @@ from .common import run_cmd, read_json
 log = logging.getLogger(__name__)
 # -------------
 
+def move_window(wid, x, y, w, h, title, verbose=False):
+    """
+    Move the window specified by is (wid) to the new x, y offset and
+    the new width (w) and height (h)
+
+    $ wmctrl -ir {wid} -e 0,{x},{y},{w},{h}
+
+    # Parameters
+
+    wid:str
+        - The id of the window to move
+
+    x:int
+        - x-coordinate of the upper left corner
+
+    y:int
+        - y-coordinate of the upper left corner
+
+    w:int
+        - width of the window
+
+    h:int
+        - height of the window
+
+    title:str
+        - The title information of the window
+
+    verbose:bool
+        - used to indicate we want more verbose information
+
+    """
+
+    if verbose:
+        log.info(f'wmctrl -ir {wid} -e 0,{x},{y},{w},{h} "  -> {title}"')
+
+    # restore the correct size and position on the desktop
+    results = run_cmd([
+        'wmctrl',
+        '-ir', wid,
+        '-e', f'0,{x},{y},{w},{h}',
+    ])
+
+    for r in results:
+        log.info(r)
+
+def set_desktop(wid, deskid, title, verbose=False):
+    """
+    Given the window (wid) move it to the virtual desktop (deskid)
+
+    $ wmctrl -ir {wid} -t {deskid}
+
+    # Parameters
+
+    wid:str
+        - The id of the window to move
+
+    title:str
+        - The title information of the window
+
+    verbose:bool
+        - used to indicate we want more verbose information
+    """
+
+    if verbose:
+        log.info(f'wmctrl -ir {wid} -t {deskid} " -> {title}"')
+
+    results = run_cmd([
+        'wmctrl',
+        '-ir', wid,
+        '-t', deskid,
+    ])
+
+    for r in results:
+        log.info(r)
+
+def position_adjustments(x, y, scale_x, scale_y, title):
+    """
+
+    Given the window x,y corner offset and the window title,
+    determine if the coordinates need to be adjusted.
+
+    # Parameters
+
+    x:int
+        - x-coordinate of the upper left corner
+
+    y:int
+        - y-coordinate of the upper left corner
+
+    scale_x:int
+        - The scale factor to apply to the x coordinates in some cases
+
+    scale_y:int
+        - The scale factor to apply to the y coordinates in some cases
+
+    title:str
+        - The title information of the window
+
+    """
+
+    if 'Firefox' in title:
+
+        # apply a kludge to get it to restore properly...
+
+        x -= 7
+        y -= 8
+
+    elif 'Sublime Text' in title:
+
+        # apply a kludge to get it to restore properly...
+
+        x -= 10
+        y -= 82
+
+    elif 'Discord' in title:
+
+        # apply a kludge to get it to restore properly...
+
+        x -= 10
+        # y = math.floor(y*scale_y) + 121
+
+    else:
+
+        x = math.floor(x*scale_x)
+        y = math.floor(y*scale_y)
+
+    return x, y
+
 @click.command('restore')
-@click.option('--log',
+@click.option('--verbose',
               is_flag=True,
-              help='Display the list of saved windows to STDOUT.')
+              help='Display more verbose output.')
 @click.pass_context
 def restore(*args, **kwargs):
     """
@@ -62,6 +190,21 @@ def restore(*args, **kwargs):
         log.info('Nothing to restore!')
 
         return
+
+    # ----------------------
+    # JSON data structure
+    #   [
+    #   "0x02600008",  # Window ID (Hex)
+    #   "2",           # Desktop number
+    #   "70",          # x-offset
+    #   "460",         # y-offset
+    #   "940",         # width
+    #   "500",         # height
+    #   "vidar",
+    #   "\ud83c\udfe1-parents-only",
+    #   "-",
+    #   "Discord"
+    # ]
 
     # Determine the scale factors for the x and y position
     # The scale factor will help restore the windows to the correct position given the system has
@@ -81,67 +224,20 @@ def restore(*args, **kwargs):
 
         title = " ".join(title)
 
-        if kwargs['log']:
-            log.info(f'Restoring: id={wid}, x={x}, y={y}, w={w}, h={h}, {title}')
+        x,y = position_adjustments(int(x), int(y), scale_x, scale_y, title)
 
-        x = int(x)
-        y = int(y)
+        # --------------
+        # 1. move to the correct position on the active desktop
 
-        if 'Firefox' in title:
+        move_window(wid, x, y, w, h, title, verbose=kwargs['verbose'])
 
-            # apply a kludge to get it to restore properly...
 
-            x -= 7
-            y -= 8
+        # --------------
+        # 2. move to the correct desktop once it is in the correct position
 
-        elif 'Sublime Text' in title:
+        set_desktop(wid, deskid, title, verbose=kwargs['verbose'])
 
-            # apply a kludge to get it to restore properly...
 
-            x = math.floor(x*scale_x) + 2
-            y = math.floor(y*scale_y) + 121
-
-        elif 'Discord' in title:
-
-            # apply a kludge to get it to restore properly...
-
-            x -= 10
-            # y = math.floor(y*scale_y) + 121
-
-        else:
-
-            x = math.floor(x*scale_x)
-            y = math.floor(y*scale_y)
-
-        # w = int(w)
-        # h = int(h)
-
-        if kwargs['log']:
-            log.info(f'scaled: id={wid}, x={x}, y={y}, w={w}, h={h}, {" ".join(p[6:])}')
-            log.info(f'wmctrl -ir {wid} -e 0,{x},{y},{w},{h}')
-
-        results = run_cmd([
-            'wmctrl',
-            '-ir', wid,
-            '-e', f'0,{x},{y},{w},{h}',
-        ])
-
-        for r in results:
-            log.info(r)
-
-    # The data structure:
-    #   [
-    #   "0x02600008",  # Window ID (Hex)
-    #   "2",           # Desktop number
-    #   "70",          # x-offset
-    #   "460",         # y-offset
-    #   "940",         # width
-    #   "500",         # height
-    #   "vidar",
-    #   "\ud83c\udfe1-parents-only",
-    #   "-",
-    #   "Discord"
-    # ]
 
     # We are going to use `$ wmctrl -ir wid -e 0,x,y,w,h` to restore the window position
 
