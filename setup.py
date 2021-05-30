85ee2deae2ce1de3cb0798fd6a404e67f3eb1835
Troy Williams <troy.williams@bluebill.net>
2021-05-30 11:44:42 -0400

    refactor.

--- setup.py
+++ setup.py
@@ -28,13 +28,14 @@ setup(
     author="Troy Williams",
     author_email="troy.williams@bluebill.net",
     description="Remember the position of your open windows and restore them at a later point.",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    python_requires=">=3.9",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     include_package_data=True,
     install_requires=[
         "click",
@@ -45,5 +46,4 @@ setup(
             'hindsight=hindsight.hindsight:main',
         ],
     },
-    python_requires=">=3.9",
 )
